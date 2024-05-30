9119a73d5fc7920b0111bb3fd7a6487b35d347a0
Xuan <127477394+xuanlin129@users.noreply.github.com>
2024-05-30 15:03:39 +0800

    Delete js/safari_vh.js

--- js/safari_vh.js
+++
@@ -1,9 +0,0 @@
-function safariHacks() {
-  let windowsVH = window.innerHeight / 100;
-  document.querySelector('.wrap').style.setProperty('--vh', windowsVH + 'px');
-  window.addEventListener('resize', function() {
-      document.querySelector('.wrap').style.setProperty('--vh', windowsVH + 'px');
-  });
-}
-
-safariHacks();
